1d1c7549952b32549d7d658cf84ef6173750a66f
JonasPTS <jonasroberto1000@gmail.com>
2026-05-11 20:03:01 -0300

    V9.25

--- Correio-Elegante/backend/server.js
+++ Correio-Elegante/backend/server.js
@@ -2,10 +2,16 @@ const express = require("express");
 const cors = require("cors");
 const { addRow } = require("./sheets");
 function getBrazilHour() {
-  return new Date().toLocaleTimeString("pt-BR", {
+  const now = new Date();
+
+  return now.toLocaleString("pt-BR", {
     timeZone: "America/Sao_Paulo",
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
     hour: "2-digit",
     minute: "2-digit",
+    second: "2-digit",
     hour12: false
   });
 }
