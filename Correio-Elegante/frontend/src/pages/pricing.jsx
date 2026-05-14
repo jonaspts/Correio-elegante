4cd7d5c1609e3fbb0d18424370ebbf1de69290c6
JonasPTS <jonasroberto1000@gmail.com>
2026-05-14 19:08:31 -0300

    V9.83a

--- Correio-Elegante/frontend/src/pages/pricing.jsx
+++ Correio-Elegante/frontend/src/pages/pricing.jsx
@@ -114,43 +114,6 @@ export default function Pricing({ goToHome }) {
       alert("Envie o comprovante do Pix");
       return;
     }
-const auth = new google.auth.GoogleAuth({
-  credentials: JSON.parse(process.env.GOOGLE_CREDS),
-  scopes: ["https://www.googleapis.com/auth/spreadsheets"],
-});
-
-const spreadsheetId = "12_DPoKN-kQV6oP9PCz_Xg7cEekbb3VfwekbSfLDBmAI";
-
-async function addRow(data) {
-  const client = await auth.getClient();
-
-  const googleSheets = google.sheets({
-    version: "v4",
-    auth: client,
-  });
-
-  console.log("DADOS RECEBIDOS NO SHEETS:", data);
-
-  await googleSheets.spreadsheets.values.append({
-    spreadsheetId,
-    range: "A:G",
-    valueInputOption: "USER_ENTERED",
-    requestBody: {
-      values: [[
-        data.dataHora || new Date().toLocaleString("pt-BR"),
-        data.de || "",
-        data.para || "",
-        data.receiverClassroom || "",
-        data.item || "",
-        data.mensagem || "",
-        data.pagamento || ""
-      ]]
-    }
-  });
-}
-
-module.exports = { addRow };
-
     try {
       setLoading(true);
 
